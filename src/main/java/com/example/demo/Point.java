dcc8d3c014f2d1cba4af359804d665bb2297e01f
Otis <phu.xv97@gmail.com>
2021-08-11 16:38:22 +0700

    add again

---
+++ src/main/java/com/example/demo/Point.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Point {
+
+    private double point;
+
+    private double math;
+
+    private double english;
+
+
+
+}
